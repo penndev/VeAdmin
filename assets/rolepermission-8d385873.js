4b8805767438f3adf2f92d771402305cf48d2573
penndev <penndev@users.noreply.github.com>
2023-12-01 15:33:23 +0000

    deploy: 02bcc241470959f6a64e48e6ca0e7f0842353545

---
+++ assets/rolepermission-8d385873.js
@@ -0,0 +1,1 @@
+import{s as t}from"./request-f2f54ad8.js";const o=e=>t({url:"/system/role",method:"get",params:e}),r=e=>t({url:"/system/role",method:"post",data:e}),l=e=>t({url:"/system/role",method:"put",data:e}),m=e=>t({url:"/system/role",method:"delete",params:e});export{l as a,m as d,o as g,r as p};
